9671415b3c00ccda96eae6d9889b59945cde357b
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-05-18 19:41:58 +0530

    feat: count lines

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::ErrorKind;
 use std::path;
+use std::process::exit;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -12,24 +13,78 @@ struct Args {
     lines: Option<usize>,
 
     #[clap(short = 'c', long)]
-    bytes: Option<usize>
+    bytes: Option<usize>,
+}
+
+struct HeadOp {
+    files: Vec<path::PathBuf>,
+    lines: Option<usize>,
+    bytes: Option<usize>,
+}
+
+impl HeadOp {
+    fn new(files: Vec<path::PathBuf>, lines: Option<usize>, bytes: Option<usize>) -> Self {
+        Self {
+            files,
+            lines,
+            bytes,
+        }
+    }
+
+    fn head(&mut self) -> Option<String> {
+        match (self.lines, self.bytes) {
+            (Some(_), Some(_)) => {
+                println!("head-rs: can\'t combine line and byte counts");
+                exit(1)
+            }
+            (None, None) => self.lines = Some(10),
+            _ => (),
+        }
+
+        if self.files.len() == 0 {
+            println!("head-rs: No files specified");
+            exit(1);
+        }
+
+        for file in &self.files {
+            if let Some(_) = self.lines {
+                return Some(fs::read_to_string(file)
+                    .unwrap()
+                    .lines()
+                    .take(self.lines.unwrap())
+                    .collect::<Vec<&str>>()
+                    .join("\n"));
+            }
+        }
+
+        None
+    }
 }
 
 fn main() {
     let args = Args::parse();
-    let filenames = args.filenames.clone();
+    let mut filenames = args.filenames.clone();
 
-    for filename in filenames {
+    for filename in filenames.clone() {
         match File::open(&filename) {
-            Ok(_) => println!("file exists!"),
+            Ok(_) => continue,
             Err(e) => match e.kind() {
-                ErrorKind::NotFound => println!("No such file: {}", &filename.to_str().unwrap()),
+                ErrorKind::NotFound => {
+                    println!("head-rs: No such file: {}", &filename.to_str().unwrap());
+                    filenames = filenames
+                        .into_iter()
+                        .filter(|f| f.to_str() != filename.to_str())
+                        .collect();
+                }
                 _ => println!(
-                    "Error while opening file {}: {}",
+                    "head-rs: Error while opening file {}: {}",
                     &filename.to_str().unwrap(),
                     e
                 ),
             },
         }
     }
+
+    let mut head_op = HeadOp::new(filenames, args.lines, args.bytes);
+    println!("{}", head_op.head().unwrap());
 }
